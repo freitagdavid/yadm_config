4512fb0a951beb34ee8336383335ade602a3a1cd
David Freitag (drfreitag) <drfreitag@sbgtv.com>
2022-11-01 14:24:18 -0400

    cleanup files a bit more

---
+++ .config/zsh/config.d/env.zsh
@@ -0,0 +1,13 @@
+#Android Dev Stuff
+
+export ZSH="/Users/drfreitag/.oh-my-zsh"
+
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/tools/bin
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-11.jdk/Contents/Home
+export PATH="/usr/local/opt/openjdk/bin:$PATH"
+
+export EDITOR='nano'
