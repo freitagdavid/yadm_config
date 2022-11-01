a5fe3df1133a064a044420d04a6bfb78fee55be1
David Freitag (drfreitag) <drfreitag@sbgtv.com>
2022-11-01 14:05:08 -0400

    first

---
+++ .config/zsh/config.d/rn_helpers.zsh
@@ -0,0 +1,65 @@
+##################################################
+##                  RN Helpers                  ##
+##################################################
+
+function cleanNode() {
+    rm -rf ./node_modules &
+    progress -mp $!
+    rm package-lock.json
+    rm -rf ./example/node_modules &
+    progress -mp $!
+    rm ./example/yarn.lock
+    rm ./example/yarn-error.log
+}
+
+function cleanupAndroid() {
+    cd ./example/android
+    ./gradlew clean
+    cd ../..
+}
+
+function nukePods() {
+    cd ./example/ios
+    rm -rf ~/Library/Caches/CocoaPods &
+    progress -mp $!
+    rm -rf Pods &
+    progress -mp $!
+    rm -rf ~/Library/Developer/Xcode/DerivedData/* &
+    progress -mp $!
+    pod deintegrate
+    pod setup
+    cd ../..
+}
+
+function setupProject() {
+    npm i
+    cd example
+    yarn install
+    cd ios
+    pod install
+    cd ../..
+}
+
+function cleanupWatchman() {
+    watchman watch-del ./
+}
+
+function nukeAndSetup() {
+    cleanNode
+    nukePods
+    cleanupAndroid
+    cleanupWatchman
+    setupProject
+}
+
+function and() {
+    cd example
+    yarn run android
+    cd ..
+}
+
+function ios() {
+    cd example
+    yarn run ios
+    cd ..
+}
